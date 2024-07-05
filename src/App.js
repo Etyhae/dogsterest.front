24512073493eb91358139a79b7b2c652672b7eeb
Etyhae <max.mand@yandex.ru>
2024-07-05 16:09:06 +0300

    feature: infinite scroll images

--- src/App.js
+++ src/App.js
@@ -1,33 +1,72 @@
 import './App.css';
-import { ImageList, ImageListItem } from '@mui/material';
-import { useGetRandomDogQuery } from './services/dogs';
+import { Box, CircularProgress } from '@mui/material';
+import { useGetSomeDogsQuery } from './services/dogs';
+import { useState, useEffect, useCallback } from 'react';
 
 function App() {
+  const [dogImagesList, setDogImagesList] = useState([]);
+  const [count, setCount] = useState(24);
+  const { data, error, isLoading } = useGetSomeDogsQuery(count);
 
-  const { data, error, isLoading } = useGetRandomDogQuery()
+  useEffect(() => {
+    if (data) {
+      setDogImagesList(prevList => [...prevList, ...data]);
+    }
+  }, [data]);
+
+  const debounce = useCallback((callback, delay = 500) => {
+    let timer = null;
+    return (...args) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    };
+  }, []);
+
+  const handleScrollToBottom = useCallback((e) => {
+    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    console.log(bottom, isLoading);
+    if (bottom && !isLoading) {
+      setCount(prevCount => prevCount === 12 ? 9 : 12)
+    }
+  }, [setCount, isLoading])
+
+  const debouncedHandleScroll = debounce(handleScrollToBottom);
+
   return (
     <div className="App">
       <main style={{ maxWidth: '100vw' }}>
-
-        <ImageList variant="masonry" cols={3} gap={8}>
-          {error ? (
-            <>Oh no, there was an error</>
-          ) : isLoading ? (
-            <>Loading...</>
-          ) : data ? (
-            <ImageListItem key={data.fileSizeBytes}>
-              <img
-                srcSet={`${data.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                src={`${data.url}?w=248&fit=crop&auto=format`}
-                alt={''}
-                loading="lazy"
-                className='img-rounded'
-              />
-            </ImageListItem>
-          ) : null}
-        </ImageList>
+        <Box
+          style={{ height: '100vh', overflowY: 'auto' }}
+          onScroll={debouncedHandleScroll}
+        >
+          <div className='masonry'>
+            {error ? (
+              <>Oh no, there was an error</>
+            ) : isLoading ? (
+              <div style={{ position: 'absolute', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%', zIndex: '9999' }}>
+                <CircularProgress />
+              </div>
+            ) : dogImagesList ? (
+              dogImagesList.map((item, index) => (
+                <div key={index} className='masonry__block'>
+                  <img
+                    srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    src={`${item.url}?w=248&fit=crop&auto=format`}
+                    alt={''}
+                    loading="lazy"
+                    className='img-rounded '
+                  />
+                </div>
+              ))
+            ) : null}
+          </div>
+        </Box>
       </main>
-    </div >
+    </div>
   );
 }
 
--- src/services/dogs.js
+++ src/services/dogs.js
@@ -7,7 +7,24 @@ export const dogsApi = createApi({
         getRandomDog: builder.query({
             query: () => `woof.json`,
         }),
+        getSomeDogs: builder.query({
+            async queryFn(count, _queryApi, _extraOptions, baseQuery) {
+                try {
+                    const results = [];
+                    for (let i = 0; i < count; i++) {
+                        const result = await baseQuery('woof.json');
+                        if (result.error) {
+                            return { error: result.error };
+                        }
+                        results.push(result.data);
+                    }
+                    return { data: results };
+                } catch (error) {
+                    return { error: { status: 'Error fetching several dogs', message: error.message } };
+                }
+            },
+        }),
     }),
 })
 
-export const { useGetRandomDogQuery } = dogsApi
+export const { useGetRandomDogQuery, useGetSomeDogsQuery } = dogsApi;
