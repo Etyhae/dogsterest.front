3f467daa16410f008574b552c982dbfd0e582b16
Etyhae <max.mand@yandex.ru>
2024-07-08 14:24:13 +0300

    feature: add likes func

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import './App.css';
-import { Box, CircularProgress } from '@mui/material';
+import { Box, CircularProgress, ToggleButton, ToggleButtonGroup, ImageList, ImageListItem } from '@mui/material';
 import { useGetSomeDogsQuery } from './services/dogs';
 import { useState, useEffect, useCallback } from 'react';
+import { FavoriteBorderOutlined, HomeOutlined } from '@mui/icons-material';
 
 function App() {
   const [dogImagesList, setDogImagesList] = useState([]);
   const [count, setCount] = useState(24);
   const { data, error, isLoading } = useGetSomeDogsQuery(count);
+  const [currentView, setCurrentView] = useState('feed');
+  const pastLikes = JSON.parse(localStorage.getItem('likedPosts')) || [];
+  const [likedPosts, setLikedPosts] = useState(pastLikes);
 
   useEffect(() => {
     if (data) {
       setDogImagesList(prevList => [...prevList, ...data]);
     }
   }, [data]);
+
+  useEffect(() => {
+    if (likedPosts.length > 0) {
+      localStorage.setItem('likedPosts', JSON.stringify(likedPosts));
+    }
+  }, [likedPosts]);
+
+  const handleLike = (e) => {
+    const url = e.target.getAttribute('data-source');
+    setLikedPosts(prevLiked => (prevLiked.includes(url) ? prevLiked : [...prevLiked, url]));
+  };
 
   const debounce = useCallback((callback, delay = 500) => {
     let timer = null;
@@ -38,32 +53,65 @@ function App() {
 
   return (
     <div className="App">
-      <main style={{ maxWidth: '100vw' }}>
+      <main style={{ maxWidth: '100vw', overflowY: 'visible' }}>
+        <Box display="flex"
+          alignItems="center" justifyContent={'center'} sx={{ height: '5rem', width: '100%' }}>
+          <ToggleButtonGroup sx={{ color: '#FFFFFF' }}
+            value={currentView} exclusive onChange={() => (currentView === 'feed' ? setCurrentView('liked') : setCurrentView('feed'))} aria-label='show liked'>
+            <ToggleButton color='success' sx={{ color: 'gray' }} value={'feed'}>
+              <HomeOutlined />
+            </ToggleButton>
+            <ToggleButton color='success' sx={{ color: 'gray' }} value={'liked'}>
+              <FavoriteBorderOutlined />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
         <Box
-          style={{ height: '100vh', overflowY: 'auto' }}
+          style={{ width: '80vw', margin: '0 auto', height: '100%', overflowY: 'auto', textAlign: 'center', position: 'relative' }}
           onScroll={debouncedHandleScroll}
         >
-          <div className='masonry'>
+          {isLoading && (
+            <div style={{ position: 'absolute', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%', zIndex: '9999', backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
+              <CircularProgress />
+            </div>
+          )}
+          <ImageList variant="masonry" cols={3} gap={8}>
             {error ? (
               <>Oh no, there was an error</>
-            ) : isLoading ? (
-              <div style={{ position: 'absolute', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%', zIndex: '9999' }}>
-                <CircularProgress />
-              </div>
-            ) : dogImagesList ? (
-              dogImagesList.map((item, index) => (
-                <div key={index} className='masonry__block'>
-                  <img
-                    srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                    src={`${item.url}?w=248&fit=crop&auto=format`}
-                    alt={''}
-                    loading="lazy"
-                    className='img-rounded '
-                  />
-                </div>
-              ))
-            ) : null}
-          </div>
+            ) : (
+              currentView === 'feed' ? (
+                dogImagesList.map((item) => (
+                  <ImageListItem key={item.url}>
+                    <img
+                      srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                      src={`${item.url}?w=248&fit=crop&auto=format`}
+                      alt={''}
+                      data-source={item.url}
+                      loading="lazy"
+                      className='img-rounded'
+                      onClick={handleLike}
+                    />
+                  </ImageListItem>
+                ))
+              ) : likedPosts.length > 0 ? (
+                likedPosts.map((item, index) => (
+                  <ImageListItem key={index}>
+                    <img
+                      srcSet={`${item}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                      src={`${item}?w=248&fit=crop&auto=format`}
+                      alt={''}
+                      loading="lazy"
+                      className='img-rounded '
+                    />
+                  </ImageListItem>
+                ))
+              ) : (
+                <ImageListItem cols={3}>
+                  <p>Вы еще не выбрали то, что вам нравится</p>
+                </ImageListItem>
+              )
+            )}
+          </ImageList>
         </Box>
       </main>
     </div>
